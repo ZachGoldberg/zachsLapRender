4b3f3da4eeab5a3b1cf69306d0d5686269e5ff94
Zachary Goldberg <zach@zachgoldberg.com>
2016-08-24 22:51:28 -0700

    Internal data structure outline

--- src/models.py
+++ src/models.py
@@ -1,3 +1,4 @@
+
 class LapList(object):
     pass
 
@@ -11,4 +12,47 @@ class Lap(object):
     pass
 
 class Fix(object):
-    pass
+    FIX_ID = "fix_id"
+    FUEL = "fuel"
+    ALT_FT = "alt_ft"
+    ACCEL_SOURCE = "accel_source"
+    HDOP = "hdop"
+    ODB_KPH = "odb_kph"
+    LAP_TIME = "lap_time"
+    MAF = "maf"
+    LONG = "long"
+    ACCURACY = "accuracy"
+    SATELITES = "satelites"
+    ALT_M = "alt_m"
+    LIN_G = "lin_g"
+    DATE = "date"
+    LEAN = "lean"
+    IAT_C = "iat_c"
+    DISTANCE_MI = "distance_mi"
+    GEAR = "gear"
+    MAP = "map"
+    OIL_C = "oil_c"
+    THROTTLE = "throttle"
+    SPEED_MPH = "speed_mph"
+    LAP_INDEX = "lap_index"
+    HEADING_DEG = "heading_deg"
+    COOLANT_C = "coolant_c"
+    GPS_FIX_TYPE = "gps_fix_type"
+    LAT_G = "lat_g"
+    GPS_DIFF = "gps_diff"
+    DISTANCE_KM = "distance_km"
+    ODB_MPH = "odb_mph"
+    WALL_TIME = "wall_time"
+    LAT = "lat"
+    RPM = "rpm"
+    SPEED_KPH = "speed_kph"
+    ATTRS = [FIX_ID, FUEL, ALT_FT, ACCEL_SOURCE, HDOP, ODB_KPH, LAP_TIME, MAF, LONG,
+             ACCURACY, SATELITES, ALT_M, LIN_G, DATE, LEAN, IAT_C, DISTANCE_MI, GEAR, MAP,
+             OIL_C, THROTTLE, SPEED_MPH, LAP_INDEX, HEADING_DEG, COOLANT_C, GPS_FIX_TYPE, LAT_G,
+             GPS_DIFF, DISTANCE_KM, ODB_MPH, WALL_TIME, LAT, RPM, SPEED_KPH]
+
+    def setattr(self, attr, val):
+        if attr not in self.ATTRS:
+            raise Exception("Invalid Attr")
+
+        setattr(self, attr, val)
--- src/parsers/harrys_csv.py
+++ src/parsers/harrys_csv.py
@@ -3,6 +3,54 @@ from . import LaptimeParser
 
 class HarrysCSVParser(LaptimeParser):
 
+    COL_MAPPING = {
+        "INDEX": "fix_id",
+        "LAPINDEX": "lap_index",
+        "DATE": "date",
+        "TIME": "wall_time",
+        "TIME_LAP": "lap_time",
+        "LATITUDE": "lat",
+        "LONGITUDE": "long",
+        "SPEED_KPH": "speed_kph",
+        "SPEED_MPH": "speed_mph",
+        "HEIGHT_M": "alt_m",
+        "HEIGHT_FT": "alt_ft",
+        "HEADING_DEG": "heading_deg",
+        "GPSDIFFERENTIAL[UNKNOWN/2D3D/DGPS/INVALID]": "gps_diff",
+        "GPSFIX[NOFIX/2D/3D/UNKNOWN]": "gps_fix_type",
+        "SATELLITES": "satelites",
+        "HDOP": "hdop",
+        "ACCURACY_M": "accuracy",
+        "DISTANCE_KM": "distance_km",
+        "DISTANCE_MILE": "distance_mi",
+        "ACCELERATIONSOURCE[CALCULATED/MEASURED/UNDEFINED]": "accel_source",
+        "LATERALG": "lat_g",
+        "LINEALG": "lin_g",
+        "LEAN": "lean",
+        "RPM": "rpm",
+        "MAF": "maf",
+        "WHEEL_SPEED_KPH": "odb_kph",
+        "WHEEL_SPEED_MPH": "odb_mph",
+        "THROTTLE": "throttle",
+        "GEAR": "gear",
+        "FUEL": "fuel",
+        "COOLANT_CELSIUS": "coolant_c",
+        "OIL_CELSIUS": "oil_c",
+        "IAT_CELSIUS": "iat_c",
+        "MAP": "map",
+    }
+
+
     @classmethod
     def parse_data(cls, data):
-        pass
+        """head -n 3 sample.csv
+        Harry's GPS LapTimer
+INDEX,LAPINDEX,DATE,TIME,TIME_LAP,LATITUDE,LONGITUDE,SPEED_KPH,SPEED_MPH,HEIGHT_M,HEIGHT_FT,HEADING_DEG,GPSDIFFERENTIAL[UNKNOWN/2D3D/DGPS/INVALID],GPSFIX[NOFIX/2D/3D/UNKNOWN],SATELLITES,HDOP,ACCURACY_M,DISTANCE_KM,DISTANCE_MILE,ACCELERATIONSOURCE[CALCULATED/MEASURED/UNDEFINED],LATERALG,LINEALG,LEAN,RPM,MAF,WHEEL_SPEED_KPH,WHEEL_SPEED_MPH,THROTTLE,GEAR,FUEL,COOLANT_CELSIUS,OIL_CELSIUS,IAT_CELSIUS,MAP
+57358,381,14-AUG-16,16:57:02.65,0.000000,34.874742,-118.258734,102.600000,63.752684,740.000000,2427.821526,288.1,2,3,11,0.700000,4.9,0.000000,0.000000,1,0.15,0.34,8.500000,0,0.000000,0.000000,0.000000,0.000000,0,0.000000,0.000000,0.000000,0.000000,0.000000
+        """
+        # Fast forward past the Harry's notice
+        data = "\n".join(data.split("\n")[1:])
+
+        reader = csv.DictReader(data)
+        for fix_data in reader:
+            pass
